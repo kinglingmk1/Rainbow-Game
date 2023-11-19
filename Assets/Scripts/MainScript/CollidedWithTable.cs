35eb95ef728f5627b08d2b5da18dd2698dde6c4a
kinglingmk1 <jackymak2004@gmail.com>
2023-11-19 16:44:11 +0800

    Rainbow Collision
    Make coin can collision with rainbow

--- Assets/Scripts/MainScript/CollidedWithTable.cs
+++ Assets/Scripts/MainScript/CollidedWithTable.cs
@@ -8,12 +8,15 @@ public class CollidedWithTable : MonoBehaviour
     public Text CoinHitTableCount, OwnCoin;
     public Rigidbody rb;
     Vector3 GetSpawnPosition, vel;
+    bool tableHit, redHit, yellowHit, greenHit, blueHit, rainbowBoundaryHit;
+    float time;
     // Start is called before the first frame update
     void Start()
     {
         //Get coin position when started.
         vel = rb.velocity;
         GetSpawnPosition = Coin.transform.position;
+        time = 0f;
     }
 
     // Update is called once per frame
@@ -22,20 +25,104 @@ public class CollidedWithTable : MonoBehaviour
         //Collided with table -> out
 
         //Collided with table + red -> out
+        if (tableHit)
+        {
+            if (time >= 3)
+            {
+                isCollision();
+                time = 0;
+            } else
+            {
+                time += Time.deltaTime;
+            }
+        }
+
     }
     void OnCollisionEnter(Collision col)
     {
-        //Collision enter with anything Count
-        Debug.Log("Enter Table");
+        if (col.gameObject.tag == "RainbowTable")
+        {
+            tableHit = true;
+            Debug.Log("Enter Table");
+        }
+        else
+        {
+            if (col.gameObject.tag == "Red")
+            {
+                redHit = true;
+                Debug.Log("Enter Red");
+            }
+            else
+            {
+                if (col.gameObject.tag == "Yellow")
+                {
+                    yellowHit = true;
+                    Debug.Log("Enter Yellow");
+                }
+                else
+                {
+                    if (col.gameObject.tag == "Green")
+                    {
+                        greenHit = true;
+                        Debug.Log("Enter Green");
+                    }
+                    else
+                    {
+                        if (col.gameObject.tag == "Blue")
+                        {
+                            blueHit = true;
+                            Debug.Log("Enter Blue");
+                        }
+                    }
+                }
+            }
+        }
     }
-    void OnCollisionStay(Collision col)
-    {
-        //Debug statement
-        //Coin keep collision will in this state.
-        //Coin collision with table will count 1 to hit table on canvas.
-        CoinHitTableCount.text = (int.Parse(CoinHitTableCount.text) + 1).ToString();
-        //Coin position willl reset to start position
 
+    void OnCollisionExit(Collision col)
+    {
+        if (col.gameObject.tag == "RainbowTable")
+        {
+            tableHit = false;
+            Debug.Log("Leave Table");
+        }
+        else
+        {
+            if (col.gameObject.tag == "Red")
+            {
+                redHit = false;
+                Debug.Log("Leave Red");
+            }
+            else
+            {
+                if (col.gameObject.tag == "Yellow")
+                {
+                    yellowHit = false;
+                    Debug.Log("Leave Yellow");
+                }
+                else
+                {
+                    if (col.gameObject.tag == "Green")
+                    {
+                        greenHit = false;
+                        Debug.Log("Leave Green");
+                    }
+                    else
+                    {
+                        if (col.gameObject.tag == "Blue")
+                        {
+                            blueHit = false;
+                            Debug.Log("Leave Blue");
+                        }
+                    }
+                }
+            }
+        }
+    }
+    void isCollision()
+    {
+        //Collision check
+        CoinHitTableCount.text = (int.Parse(CoinHitTableCount.text) + 1).ToString();
         OwnCoin.text = (int.Parse(OwnCoin.text) - 1).ToString();
         if (int.Parse(OwnCoin.text) > 0)
         {
@@ -49,10 +136,5 @@ public class CollidedWithTable : MonoBehaviour
         }
 
         Debug.Log("Collision With Table");
-    }
-
-    void OnCollisionExit(Collision col)
-    {
-        
     }
 }
