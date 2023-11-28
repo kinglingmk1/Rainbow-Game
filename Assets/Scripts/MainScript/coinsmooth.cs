1678e73bca59c2b3f72c39ae0daa4ff4b7d46e57
moonlet023 <moonlet023@gmail.com>
2023-11-28 11:49:08 +0800

    test update

--- Assets/Scripts/MainScript/coinsmooth.cs
+++ Assets/Scripts/MainScript/coinsmooth.cs
@@ -7,12 +7,14 @@ public class coinsmooth : MonoBehaviour
     // Start is called before the first frame update
     public GameObject coin;
     Vector3 coinvelocity;
+    Vector3 velscale;
     
     
     void Start()
     {
         coin = this.gameObject;
         coinvelocity = coin.GetComponent<Rigidbody>().velocity;
+        velscale = new Vector3(1,2.6f,1);
     }
 
     // Update is called once per frame
@@ -25,8 +27,7 @@ public class coinsmooth : MonoBehaviour
     {
         if (coinvelocity != Vector3.zero)
         {
-            coinvelocity = coin.GetComponent<Rigidbody>().velocity;
-            coin.GetComponent<Rigidbody>().velocity = Vector3.Lerp(coinvelocity, Vector3.zero, 0.1f);
+            coin.GetComponent<Rigidbody>().velocity = Vector3.Lerp(coinvelocity, velscale, 0.1f);
         }
     }
 }
